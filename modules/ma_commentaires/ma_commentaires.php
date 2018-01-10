255c624bbfb9f72555fb2f8685f4a6ce8c7625ce
WaximeA <aveline.maxime@gmail.com>
2018-01-10 08:54:00 +0100

    #eval : Add configuration file to new ma_commentaires module

---
+++ modules/ma_commentaires/ma_commentaires.php
@@ -0,0 +1,36 @@
+<?php
+if (!defined('_PS_VERSION_')) {
+    exit;
+}
+
+class Ma_Commentaires extends Module
+{
+    public function __construct()
+    {
+        $this->name = 'ma_commentaires';
+        $this->tab = 'front_office_features';
+        $this->version = '1.0.0';
+        $this->author = 'Maxime AVELINE';
+        $this->need_instance = 0;
+        $this->ps_versions_compliancy =
+            array(
+                'min' => '1.7',
+                'max' =>
+                    _PS_VERSION_
+            );
+
+        $this->bootstrap = true;
+
+        parent::__construct();
+
+
+        $this->displayName = $this->l('MA Commentaires');
+        $this->description = $this->l('Ceci est un module de commentaire présent sur les pages produits');
+
+        $this->confirmUninstall = $this->l('Êtes-vous sûr de vouloir désinstaller le module MA Commentaire ?');
+
+        if (!Configuration::get('MACOMMENTAIRES_NAME')) {
+            $this->warning = $this->l('No name provided');
+        }
+    }
+}
